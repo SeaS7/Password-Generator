1af41c902226830ff11b824542817fb04ac2925d
Muhammad Huzaifa <dev.muhammadhuzaifa@gmail.com>
2024-01-17 01:28:03 +0500

    Added currency converter project

---
+++ currency-converter/app/hooks/getData.js
@@ -0,0 +1,15 @@
+"use client";
+const { useState, useEffect } = require("react");
+
+
+function getData(currency) {
+    const [data, setData] = useState({})
+    useEffect(() =>{
+      fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+      .then((res)=>res.json())
+      .then((res)=>setData(res[currency]))
+    }, [currency])
+    console.log(data);
+    return data
+}
+export default getData;
---
+++ currency-converter/app/page.js
@@ -0,0 +1,81 @@
+"use client";
+import React, { useState } from 'react'
+import getData from './hooks/getData';
+import InputBox from './commponects/InputBox';
+
+const page = () => {
+    const [amount, setAmount] = useState(0)
+    const [from, setFrom] = useState("usd")
+    const [to, setTo] = useState("pkr")
+    const [convertedAmount, setconvertedAmount] = useState(0)
+
+    const getCurrency = getData(from)
+    const options = Object.keys(getCurrency)
+
+    const swap = ()=>{
+      setFrom(to)
+      setTo(from)
+      setAmount(convertedAmount)
+      setconvertedAmount(amount)
+    }
+
+    const convert = ()=>{
+      setconvertedAmount(amount * getCurrency[to])
+    }
+
+  return (
+      <div
+          className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+          style={{
+              backgroundImage: `url('https://img.freepik.com/free-photo/cardano-blockchain-platform-collage_23-2150827491.jpg?w=1060&t=st=1705434636~exp=1705435236~hmac=c2376d457d7c9de1e9eeea0dec5e4a4308bb1c99ae4c11bb63d74456acdb86b6')`,
+          }}
+      >
+          <div className="w-full">
+              <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                  <form
+                      onSubmit={(e) => {
+                          e.preventDefault();
+                         convert()
+                      }}
+                  >
+                      <div className="w-full mb-1">
+                          <InputBox
+                             label="From"
+                             amount={amount}
+                             currencyOption={options}
+                             onCurrencyChange={(currency) => setFrom(currency)}
+                             selectCurrency={from}
+                             onAmountChange={(amount) => setAmount(amount)}
+
+                          />
+                      </div>
+                      <div className="relative w-full h-0.5">
+                          <button
+                              type="button"
+                              className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                          onClick={swap}
+                          >
+                              swap
+                          </button>
+                      </div>
+                      <div className="w-full mt-1 mb-4">
+                          <InputBox
+                                label="To"
+                                amount={convertedAmount}
+                                currencyOption={options}
+                                onCurrencyChange={(currency) => setTo(currency)}
+                                selectCurrency={to}
+                                amountDisable
+                          />
+                      </div>
+                      <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                          Convert
+                      </button>
+                  </form>
+              </div>
+          </div>
+      </div>
+  );}
+  
+export default page;
+
